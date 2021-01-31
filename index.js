05b5ef857a09e9dea3a6abeffc2710ca6f650a17
DomenicBeall <domenicbeall2@gmail.com>
2021-01-31 19:12:59 +1100

    Created pseudocode

---
+++ index.js
@@ -0,0 +1,15 @@
+// Load dependencies
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// Create a variable to store the html
+
+// Ask the user for the managers details and append the relevant html
+
+// Ask the user which employee type is being added
+
+// Switch statement to ask different questions depending on the employee type
+// Append a new card to the html for each response
+
+// Generate HTML based on responses
+
