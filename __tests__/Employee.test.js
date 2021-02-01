a4f28b5fca6150f7f54ad8ff5110bbc7bb86c444
Domenic Beall <domenicbeall2@gmail.com>
2021-02-01 13:39:17 +1100

    Fixed formatting of Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,14 +2,17 @@ const Employee = require("../lib/Employee");
 const employee = require("../lib/Employee");
 
 describe("Employee", () => {
-    // Positive test
-    it("Should set and get the initial values correctly", () => {
-      // Arrange
-      const emp = new Employee("Steve", 1, "domenicbeall2@gmail.com");
 
-      // Assert
-      expect(emp.getName()).toEqual("Steve");
-      expect(emp.getId()).toEqual(1);
-      expect(emp.getEmail()).toEqual("domenicbeall2@gmail.com");
+    describe("Initialisation", () => {
+
+        it("Should set and get the initial values correctly", () => {
+            const emp = new Employee("Steve", 1, "domenicbeall2@gmail.com");
+    
+            expect(emp.getName()).toEqual("Steve");
+            expect(emp.getId()).toEqual(1);
+            expect(emp.getEmail()).toEqual("domenicbeall2@gmail.com");
+        });
+
     });
+
   });
