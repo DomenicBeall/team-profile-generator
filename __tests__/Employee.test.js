0f87b488c33a0b1ca992a1be1c0e284ac60eb9d9
Domenic Beall <domenicbeall2@gmail.com>
2021-02-01 13:25:43 +1100

    Rewrote employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,15 @@
+const Employee = require("../lib/Employee");
 const employee = require("../lib/Employee");
 
-test("Employee is being constructed correctly", () => {
-    expect(new employee("Dave", "1", "dom@domail.com").getName()).toBe("Dave");
-});
+describe("Employee", () => {
+    // Positive test
+    it("Should set and get the initial values correctly", () => {
+      // Arrange
+      const emp = new Employee("Steve", 1, "domenicbeall2@gmail.com");
+
+      // Assert
+      expect(emp.getName()).toEqual("Steve");
+      expect(emp.getId()).toEqual(1);
+      expect(emp.getEmail()).toEqual("domenicbeall2@gmail.com");
+    });
+  });
